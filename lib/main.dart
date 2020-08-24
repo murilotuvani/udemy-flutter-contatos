44c023d34f794fbec53522a2e6144eca6060bf4c
Murilo Tuvani <murilo.tuvani@gmail.com>
2020-08-24 00:32:09 -0300

    Tela de edicao feita

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:contatos/ui/contact_page.dart';
 import 'package:contatos/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
+//  runApp(MaterialApp(home: HomePage()));
+  runApp(MaterialApp(
+      home: ContactPage(),
+      debugShowCheckedModeBanner: false // Para nao mostrar a faixa de debug
+      ));
 }
---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+
+import 'package:contatos/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  // Contato é opcional no construtur, por isso os {}
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  bool _userEdited = false;
+
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Pegando da superclasse
+    // se for nulo cria um
+    // senao cria um novo copiand os campos transformando em mapa
+    // e transformando em objeto novamente.
+    if (widget.contact == null) {
+      _editedContact = new Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editedContact.name ?? "Novo contato"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.save),
+        backgroundColor: Colors.red,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            GestureDetector(
+                child: Container(
+              width: 140.0,
+              height: 140.0,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: _editedContact.img != null
+                          ? FileImage(File(_editedContact.img))
+                          : AssetImage("images/person.png"))),
+            )),
+            TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: "Nome"),
+                onChanged: (text) {
+                  _userEdited = true;
+                  setState(() {
+                    _editedContact.name = text;
+                  });
+                }),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "e-mail"),
+              onChanged: (text) {
+                _userEdited = true;
+                _editedContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _phoneController,
+                decoration: InputDecoration(labelText: "Telefone"),
+                onChanged: (text) {
+                  _userEdited = true;
+                  _editedContact.phone = text;
+                },
+                keyboardType: TextInputType.phone),
+          ],
+        ),
+      ),
+    );
+  }
+}
